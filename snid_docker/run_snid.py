5d71abfe3b98478deee343e2c2130cccf3a14d83
Chris Duffy <c.jduffy@me.com>
2025-10-29 19:12:58 +0000

    Update run_snid.py

--- snid_docker/run_snid.py
+++ snid_docker/run_snid.py
@@ -117,6 +117,7 @@ async def _run_snid_task(params: Params):
     print("\n=== SNID Request ===")
     print(f"Request ID: {request_id}")
     print(f"Params: {params.dict()}\n")
+    print(os.getgid())
 
     params = params.dict()
     use_type = []
