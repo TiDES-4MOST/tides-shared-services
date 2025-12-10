be4219a872b4e28257fac93099a678f020545192
Chris Duffy <c.jduffy@me.com>
2025-12-10 10:11:08 +0000

    Update run_snid.py

--- snid_docker/run_snid.py
+++ snid_docker/run_snid.py
@@ -175,7 +175,7 @@ async def _run_snid_task(params: Params):
     # make an ascii file of the binned spectrum to run pysnid
     data_spec = np.column_stack([fl_smooth.spectral_axis.value, fl_smooth.flux.value])
 
-    binned_file_name = f"{file_spec_binned_path}{params['spectrum']}_binned.ascii"
+    binned_file_name = f"{file_spec_binned_path}{np.random.random_integers(1000)}_binned.ascii"
     np.savetxt(binned_file_name, data_spec, fmt=['%.2f','%.4e'])
 
     #run pysnid
